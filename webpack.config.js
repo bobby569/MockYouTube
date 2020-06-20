55b74f8f4a20e394885dcccb32e661cb318ffc1e
bobby569 <bobby.yczh@gmail.com>
2020-06-20 16:13:31 -0700

    Update package and resolve vulnerabilities

--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,60 @@
 const path = require('path');
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const HTMLWebapckPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	entry: './src/index.js',
-	output: {
-		path: path.resolve(__dirname, 'build'),
-		filename: '[name].js',
-		publicPath: './'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				loader: 'babel-loader',
-				exclude: /node_modules/
-			},
-			{
-				test: /\.css$/,
-				use: ExtractTextPlugin.extract({
-					fallback: 'style-loader',
-					use: 'css-loader'
-				})
-			}
-		]
-	},
-	plugins: [
-		new ExtractTextPlugin('style.css'),
-		new HTMLWebapckPlugin({
-			template: './index.html'
-		})
-	],
-	resolve: {
-		extensions: ['.js', '.jsx']
-	},
-	devServer: {
-		historyApiFallback: true,
-		noInfo: true,
-		overlay: true
-	}
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: '[name].js',
+        publicPath: './',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                    },
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: 'css-loader',
+                }),
+            },
+        ],
+    },
+    plugins: [new ExtractTextPlugin('style.css')],
+    devServer: {
+        historyApiFallback: true,
+        compress: true,
+        noInfo: true,
+        overlay: true,
+    },
 };
 
 if (process.env.NODE_ENV === 'production') {
-	module.exports.devtool = '#source-map';
-	module.exports.plugins = (module.exports.plugins || []).concat([
-		new webpack.DefinePlugin({
-			'process.env': {
-				NODE_ENV: '"production"'
-			}
-		}),
-		new webpack.optimize.UglifyJsPlugin({
-			sourceMap: true,
-			compress: {
-				warnings: false
-			}
-		}),
-		new webpack.LoaderOptionsPlugin({
-			minimize: true
-		})
-	]);
+    module.exports.devtool = '#source-map';
+    module.exports.plugins = (module.exports.plugins || []).concat([
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"',
+            },
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            sourceMap: true,
+            compress: {
+                warnings: false,
+            },
+        }),
+        new webpack.LoaderOptionsPlugin({
+            minimize: true,
+        }),
+    ]);
 }
